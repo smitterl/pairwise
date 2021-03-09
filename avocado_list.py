fa31bbb7c8f3a6fdf48c6c0f0b321b4afa97f447
Sebastian Mitterle <smitterl@redhat.com>
2021-03-09 10:52:41 +0100

    [avocado_list] generalize script
    1. Add pairwise filter to script
    2. Rename script
    3. Make additional avocado filter passing explicit
    
    Signed-off-by: Sebastian Mitterle <smitterl@redhat.com>

--- avocado_list.py
+++ avocado_list.py
@@ -1,19 +1,23 @@
 import re
 import subprocess
 import sys
-from minimal_filter import minimal
+from filters import minimal, pairwise
 
 # holds the vt only filter
 ONLY = []
 PRINT_ORIGINAL = False
 SHOW_STATS = False
+APPLY_FILTER = None
+OPTIONS = None
 
 def print_help(exit_code):
-    print(sys.argv[0] + " [-t] [-s] <--vt-only-filter argument>")
+    print(sys.argv[0] + " [-t] [-s] <-p|-m> [<vt-options>] <--vt-only-filter argument>")
     print()
+    print(" <vt-options> are additional filters enclosed by quotes, e.g. '--vt-type libvirt --vt-machine-type s390-virtio'")
     print(" <vt-only-filter> is a comma separated list of test names")
     print(" by passing flag -t the avocado command result is printed and not filtered")
     print(" by passing flag -s the number of original test names and filtered ones are shown")
+    print(" -p selects the pairwise filter selecting all available test cases with maximal coverage of pairs, -s selects the minimal filter selecting all available test cases covering at least all variants")
     sys.exit(exit_code)
 
 def parse_args():
@@ -21,23 +25,39 @@ def parse_args():
         print_help(1)
 
     global ONLY
+    global PRINT_ORIGINAL
+    global SHOW_STATS
+    global APPLY_FILTER
+    global OPTIONS
+
     ONLY = sys.argv[-1]
 
     if ONLY.startswith("-h"):
         print_help(0)
+
+    if not ("-m" in sys.argv or "-p" in sys.argv):
+        print_help(1)
     
     if "-t" in sys.argv: 
-        global PRINT_ORIGINAL
         PRINT_ORIGINAL = True
 
     if "-s" in sys.argv:
-        global SHOW_STATS
         SHOW_STATS = True
+
+    if "-p" in sys.argv:
+        APPLY_FILTER = pairwise.pairwise
+
+    if "-m" in sys.argv:
+        APPLY_FILTER = minimal.minimal
+
+    if "--" in sys.argv[-2]:
+        OPTIONS = sys.argv[-2]
+
 
 def get_avocado_list():
     test_names = []
-    command = ("avocado list --vt-type libvirt --vt-machine-type s390-virtio"
-               " --vt-only-filter %s --paginator off" % ONLY)
+    command = ("avocado list %s --vt-only-filter %s --paginator off" %
+               (OPTIONS, ONLY))
     output = subprocess.check_output(command, shell=True).decode()
     for row in output.split('\n'):
         if not row.startswith("VT"):
@@ -54,7 +74,7 @@ if __name__ == '__main__':
             print(name)
         sys.exit(0)
 
-    filtered_test_names = minimal(test_names)
+    filtered_test_names = APPLY_FILTER(test_names)
     for name in filtered_test_names:
         print(name)
 
